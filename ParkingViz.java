427db8605c7e0141f86bca0f015ead2ef47ac33e
tdambrin <thomas.dambrin@gmail.com>
2021-12-15 10:50:41 +0100

    empty function core

---
+++ ParkingViz.java
@@ -0,0 +1,18 @@
+import java.awt.Graphics ;
+import javax.swing.JFrame ;
+
+public class ParkingViz extends JFrame {
+    private Parking parking;
+
+    public ParkingViz() {
+        //code here
+    }
+
+    public ParkingViz(Parking parking) {
+        //code here
+    }
+
+    public void paint(Graphics g) {
+        //code here
+    }
+}
